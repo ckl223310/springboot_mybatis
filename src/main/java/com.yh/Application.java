16776da15f478e3d2bae9d9708884c8d978c1c76
CZH <15202223962@163.com>
2019-01-11 16:13:40 +0800

    mysql

--- src/main/java/com.yh/Application.java
+++ src/main/java/com.yh/Application.java
@@ -10,7 +10,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Application {
 
     /**
-     * main
      * @param args
      */
     public static void main(String[] args) {
